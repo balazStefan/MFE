549ca7018d001669a332088f3edbeb7f4dd64c09
Štefan Baláž <balazs@cngroup.dk>
2024-11-16 13:29:46 +0100

    prod configs for webpack

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,19 +1,27 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 module.exports ={
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.m?js$/, // whenever .mjs or .js will be processed by Babel
-                exclude:/node_modules/, // exclude node_modules
-                use:{
-                    loader:'babel-loader', // use babel-loader
-                    options:{
-                        presets:['@babel/preset-env','@babel/preset-react'],
-                        plugins:[
+                test: /\.m?js$/, // whenever .mjs or .js will be processed by Babel
+                exclude: /node_modules/, // exclude node_modules
+                use: {
+                    loader: 'babel-loader', // use babel-loader
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: [
                             '@babel/plugin-transform-runtime'
                         ]
                     }
                 }
+
             }
         ]
-    }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
 }
